d5f06fdedfb389937737854bcd50c2186cb1a4aa
Michael Munsie <mikemunstrocity@gmail.com>
2023-08-27 20:59:58 -0500

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ git push --tags
 PACKAGE_VERSION=`jq -r .version package.json`
 
 npm run build
-echo "{\"version\": \"${PACKAGE_VERSION}\" }" > app/dist/version.json
+echo "{\"version\": \"${PACKAGE_VERSION}\" }" > ./dist/version.json
 aws s3 rm s3://locations.munstrography.com --recursive --exclude "locations/*"
-aws s3 sync app/dist s3://locations.munstrography.com
+aws s3 sync ./dist s3://locations.munstrography.com
 aws s3 cp s3://locations.munstrography.com/index.html s3://locations.munstrography.com/index.html --metadata-directive REPLACE --cache-control max-age=0 --content-type "text/html"
