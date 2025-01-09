0039e89cded152b576a0bd1a77503afc05ff7a03
Michael Munsie <mikemunstrocity@gmail.com>
2025-01-08 21:53:06 -0600

    Improve homescreen

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="geoLocationStatus === 'none'">
-    <h1>Locations</h1>
+    <h1>DFW Car Spots</h1>
     <div style="text-align: left; padding: 15px">
       <p>
         Ready to find some neat locations for car photography in the DFW area?
@@ -10,20 +10,42 @@
         <button
           @click="init"
           class="mt-7 mb-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          style="width: 100%"
         >
           Let's Begin!
         </button>
       </div>
-      <p style="margin-top: 15px; font-size: 12px">
+      <p style="margin-top: 7.5px; margin-bottom: 15px; font-size: 12px">
         <strong>Note:</strong> We have to ask your permission for geolocation so
         we can find locations near you.
       </p>
+      <div style="display: flex; justify-content: center">
+        <img class="img" :src="`${publicPath}screenshot.png`" />
+      </div>
+      <div style="margin-top: 15px"></div>
+      <div
+        style="border-top: 1px solid #ccc; padding-top: 15px; font-size: 10px"
+      >
+        <div>&copy; {{ new Date().getFullYear() }} Michael Munsie</div>
+        <div>
+          For questions or location submission, send me a message on
+          <a href="https://www.instagram.com/munstrography">instagram</a>.
+        </div>
+      </div>
     </div>
   </div>
   <div v-if="geoLocationStatus === 'loading'">
     <h1>Locations</h1>
-    <div style="text-align: left; padding: 15px">
+    <div style="text-align: left; padding: 15px; padding-bottom: 7.5px">
       <p><i>Just a moment as we request permissions...</i></p>
+    </div>
+    <div style="text-align: left; padding-left: 15px; font-size: 12px">
+      <p>
+        <i
+          >If this doesn't work, don't panic, I'll just place you in the middle
+          of DFW in 5 seconds...</i
+        >
+      </p>
     </div>
   </div>
   <div v-else-if="geoLocationStatus === 'fail'">
